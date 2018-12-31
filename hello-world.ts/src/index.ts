6b562552ff2f718e0a4a074558d1eeb7f8d7cbf7
alex.khoroshylov <alex.khoroshylov@avira.com>
2018-12-31 14:03:56 +0100

    create session

--- hello-world.ts/src/index.ts
+++ hello-world.ts/src/index.ts
@@ -1,10 +1,23 @@
 import { Client } from "renderfarm";
 
-var client = new Client();
-client.Connect()
-    .then(function(value){
-        console.log("Hello renderfarm.js!");
-    })
-    .catch(function(err) {
-        console.error("Some error: ", err);
-    });
+async function main() {
+    var apiKey = "75f5-4d53-b0f4";
+    var workspace = "55a0bd33-9f15-4bc0-a482-17899eb67af3";
+    var host = "mel.mbnsay.com";
+    var port = 443;
+
+    var client = new Client(apiKey, workspace);
+    var workers = await client.GetWorkers(host, port);
+    console.log("Running workers: ", workers);
+
+    var session = await client.Connect(host, port);
+    return session;
+}
+
+main()
+.then(function(value){
+    console.log(value);
+})
+.catch(function(err){
+    console.error(err);
+});
